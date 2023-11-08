c4807e64289a351b38af60c90e1438087e2332f9
lullaaaby13 <sds901234@naver.com>
2023-11-08 17:52:12 +0900

    commit

--- src/pages/ingredients.vue
+++ src/pages/ingredients.vue
@@ -36,7 +36,8 @@
             rounded
             outline
             color="primary"
-            label="등록하기"
+            label="원재료 등록하기"
+            @click="registerDialog = true"
           />
         </div>
       </q-card-section>
@@ -166,6 +167,105 @@
       </q-card-section>
 
     </q-card>
+    <q-dialog v-model="registerDialog">
+      <q-card class="q-pa-md" style="width: 600px;">
+        <q-card-section>
+          <div class="text-h6">원재료 등록</div>
+          <div class="text-caption">100g당</div>
+        </q-card-section>
+
+        <q-separator/>
+
+        <q-card-section class="row">
+          <div class="col-6 q-px-sm">
+            <q-select v-model="model" :options="IngredientCategory" label="카테고리" />
+          </div>
+          <div class="col-6 q-px-sm">
+            <q-input v-model="dialogName"
+                     type="text"
+                     label="이름"
+                     :rule="[nameRules]"
+            />
+          </div>
+          <div class="col-6 q-px-sm">
+            <q-input v-model="dialogCalories"
+                     type="text"
+                     label="칼로리(Kcal)"
+                     :rule="[val => val < 0 || '0보다 커야 합니다.']"
+            />
+          </div>
+          <div class="col-6 q-px-sm">
+            <q-input v-model="dialogUnitPrice"
+                     type="text"
+                     label="단가(원)"
+            />
+          </div>
+          <div class="col-6 q-px-sm">
+            <q-input v-model="dialogCarbohydrates"
+                     type="text"
+                     label="탄수화물(g)"
+            />
+          </div>
+          <div class="col-6 q-px-sm">
+            <q-input v-model="dialogSugars"
+                     type="text"
+                     label="당류(g)"
+            />
+          </div>
+          <div class="col-6 q-px-sm">
+            <q-input v-model="dialogProtein"
+                     type="text"
+                     label="단백질(g)"
+            />
+          </div>
+          <div class="col-6 q-px-sm">
+            <q-input v-model="dialogCaffeine"
+                     type="text"
+                     label="카페인(mg)"
+            />
+          </div>
+          <div class="col-6 q-px-sm">
+            <q-input v-model="dialogFat"
+                     type="text"
+                     label="지방(g)"
+            />
+          </div>
+          <div class="col-6 q-px-sm">
+            <q-input v-model="dialogSaturatedFat"
+                     type="text"
+                     label="포화지방(g)"
+            />
+          </div>
+
+        </q-card-section>
+
+<!--        <q-separator/>-->
+
+        <q-card-section>
+          <q-input type="textarea" label="메모"/>
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="등록" color="primary" @click="onRegisterConfirmButtonClick"/>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
+    <q-dialog v-model="failRegisterIngredientDialog" persistent>
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Alert</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          같은 이름dd
+        </q-card-section>
+
+        <q-card-actions align="right">
+          <q-btn flat label="OK" color="primary" v-close-popup />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
 
     <q-dialog v-model="configDelete" persistent>
       <q-card>
@@ -196,6 +296,7 @@ const searchText = ref('');
 
 const toggle = ref(false);
 const configDelete = ref(false);
+const registerDialog = ref(true);
 
 const ingredientStore = useIngredientStore();
 const ingredients = ingredientStore.ingredients;
@@ -228,6 +329,49 @@ const resetCheckbox = () => {
 const onDeleteButtonClick = ingredient => {
   ingredientStore.delete(ingredient);
 }
+
+const dialogName = ref('');
+const dialogCalories = ref(0);
+const dialogUnitPrice = ref(0);
+const dialogCarbohydrates = ref(0);
+const dialogSugars = ref(0);
+const dialogProtein = ref(0);
+const dialogCaffeine = ref(0);
+const dialogFat = ref(0);
+const dialogSaturatedFat = ref(0);
+const failRegisterIngredientDialog = ref(false);
+const onRegisterConfirmButtonClick = () => {
+  const ingredient = {
+    name: dialogName.value,
+    calories: dialogCalories.value,
+    unitPrice: dialogUnitPrice.value,
+    carbohydrates: dialogCarbohydrates.value,
+    sugars: dialogSugars.value,
+    protein: dialogProtein.value,
+    caffeine: dialogCaffeine.value,
+    fat: dialogFat.value,
+    saturatedFat: dialogSaturatedFat.value,
+  }
+
+  const isExists = ingredientStore.exists(ingredient);
+
+  if (isExists) {
+    failRegisterIngredientDialog.value = true;
+  } else {
+    ingredientStore.save(ingredient);
+    registerDialog.value = false;
+    failRegisterIngredientDialog.value = false;
+  }
+
+}
+
+const nameRules = value => {
+  if (!value) {
+    return '이름을 입력해 주세요.';
+  }
+}
+
+
 </script>
 
 <route lang="yaml">
